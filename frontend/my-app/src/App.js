05d9fda99dd91fda325e3090c62a80f0f1fc5dd1
Ashwin <ashwin85468@gmail.com>
2024-03-06 00:49:34 +0530

    added navbar

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import Homepage from "./Pages/Homepage";
 
 function App() {
   // its search SearchBar
   return (
     <div>
-      <h1>my name is ashwin</h1>
-      <SearchBar />
+      <Homepage/>
     </div>
   );
 }
